6945addd67a0c5917de0f10a49648122ac62a309
Angel <cadops.dc@gmail.com>
2016-06-03 19:28:58 -0500

    Mi app Django Girls, segundo commit

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    # url(r'^$', 'mysite.views.home', name='home'),
+    url(r'^$', 'mysite.views.site_index'),
     url(r'^admin/', admin.site.urls),
     url(r'^blog/', include('blog.urls')),
 ]
